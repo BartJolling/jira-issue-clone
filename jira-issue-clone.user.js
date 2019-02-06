19e9d6669daf8ec73e82cfdd73e7ab1cc3f07c21
Bart Jolling <BartJolling@users.noreply.github.com>
2019-02-06 13:09:07 +0100

    Fixes #1
    inject script directly in JIRA and use JIRA.Events to add clone button at correct moments in the page lifecycle

--- jira-issue-clone.user.js
+++ jira-issue-clone.user.js
@@ -1,194 +1,228 @@
 // ==UserScript==
-// @name       Jira - Copy Paste into new ticket
+// @name       JIRA Issue - Clone
 // @namespace  http://bartjolling.github.io
-// @version    1.0.1
-// @include    *jira*/browse/*-*
-// @require    http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @version    2.0.0
+// @include    https://jira.*/*
+// @require    https://raw.githubusercontent.com/BartJolling/inject-some/master/inject-some.js
 // @grant      none
 // @run-at     document-end
 // ==/UserScript==
 
-var BROWSE_URL = "https://[your-server-url]/browse/"; 
-var API_URL = "https://[your-server-url]/rest/api/latest/issue/";
-
 /**
-* Initialize the script.
-* User-Agent must be blank to avoid JIRA issue with cross origin checks for Firefox.
-* Add the 'Clone' button when user is on the /browse/ page
-*/
-$(document).ready(function() {
-  
-    $.ajaxSetup({
-      beforeSend: function(request) {
-        request.setRequestHeader("User-Agent","");
-      }
-    });
-  
-    path = window.location.pathname;
-    
-    if( path.startsWith("/browse/")) {
-      addCloneButton();
-    }    
-});
+ * Script to be injected directly into the page
+ */
+scriptToInject = function ($) {
+	var BROWSE_URL = window.location.protocol + '//' + window.location.hostname + '/browse/';
+	var API_URL = window.location.protocol + '//' + window.location.hostname + '/rest/api/latest/issue/';
 
-/**
-* Adds a 'Clone' button to the operations toolbar in JIRA
-*/
-function addCloneButton() {  
-  try{
-    $edit = $("#opsbar-opsbar-operations");
-       
-    var $clone = $('<a>').addClass('toolbar-trigger').click(cloneIssue).append(
-          $('<span>').addClass('trigger-label').append("Clone"));
-    
-    $edit.append($('<li>').addClass('toolbar-item').append($clone));
-  }
-  catch(err) {
-    alert(err.message);
-  }
+	// User-Agent must be blank to avoid JIRA issue with cross origin checks for Firefox.
+	$.ajaxSetup({
+		beforeSend: function (request) {
+			request.setRequestHeader("User-Agent", "");
+		}
+	});
+
+	/**
+	 * Adds a 'Clone' button to the operations toolbar in JIRA
+	 */
+	function addCloneButton() {
+		try {
+			$opsbar = $("#opsbar-opsbar-operations");
+
+			var $clone = $('<a>').addClass('toolbar-trigger').click(cloneIssue).append(
+				$('<span>').addClass('trigger-label').append("Clone"));
+
+			var $spinner = $('<div>').addClass('button-spinner').css({ position: 'relative', top: '15px' });
+
+			$opsbar.append($('<li>').addClass('toolbar-item').append($clone));
+			$opsbar.append($('<li>').addClass('toolbar-item').append($spinner));
+		}
+		catch (err) {
+			console.log('[jira-issue-clone][addCloneButton] ' + err.message);
+		}
+	};
+
+	/**
+	* Main function that is execute when user clicks the clone button.
+	* Drives the process of retrieving info, building a new issue and submitting it.
+	*/
+	function cloneIssue() {
+		try {
+			$('.button-spinner').spin();
+			var issueId = $("meta[name='ajs-issue-key']").attr("content");
+
+			getCurrentIssueFields(issueId)
+				.then(function (issue) {
+					var currentFields = issue.fields;
+					var projectId = currentFields.project.id;
+					var issueTypeId = currentFields.issuetype.id;
+
+					getIssueTypeFields(projectId, issueTypeId)
+						.then(function (meta) {
+							var issueTypeFields = meta.projects[0].issuetypes[0].fields;
+							createNewIssue(currentFields, issueTypeFields);
+						})
+						.error(handleAjaxError);
+				})
+				.error(handleAjaxError);
+		}
+		catch (err) {
+			console.log('[jira-issue-clone][cloneIssue] ' + err.message);
+			$('.button-spinner').spinStop();
+			alert(err.message);
+		}
+	};
+
+	/**
+	* Returns a promise to load the data of the current issue.
+	* @param {string} issueId - id of the issue to retrieve
+	*/
+	function getCurrentIssueFields(issueId) {
+		try {
+			var query = API_URL + issueId;
+			return $.getJSON(query);
+		}
+		catch (err) {
+			alert(err.message);
+		}
+	};
+
+	/**
+	* Returns a promise to load the fields on the create screen for a certain project and issue type.
+	* @param {string} projectId - id of the project to find the Create screen.
+	* @param {string} issueTypeId - id of the issue type to find the Create screen
+	*/
+	function getIssueTypeFields(projectId, issueTypeId) {
+		try {
+			var query = API_URL + "createmeta?projectIds=" + projectId + "&issuetypeIds=" + issueTypeId + "&expand=projects.issuetypes.fields";
+			return $.getJSON(query);
+		}
+		catch (err) {
+			alert(err.message);
+		}
+	};
+
+	/**
+	* Builds a new issue using information of the current issue, filtered down with the fields on the create screen.
+	* @param {string} currentFields - list of fields of the current issue.
+	* @param {string} issueTypeFields - list of fields on the Create screen for this issue type
+	*/
+	function createNewIssue(currentFields, issueTypeFields) {
+
+		try {
+			var newIssue = {
+				"fields": {
+					"issuetype": {
+						"id": currentFields.issuetype.id
+					},
+					"priority": {
+						"id": currentFields.priority.id
+					},
+					"project": {
+						"id": currentFields.project.id
+					},
+					"summary": currentFields.summary,
+					"description": currentFields.description,
+				}
+			};
+
+			//Custom fields    
+			for (var key in issueTypeFields) {
+				if (key.startsWith('customfield_') && currentFields.hasOwnProperty(key)) {
+
+					var value = customizeValue(key, currentFields[key])
+
+					if (value) {
+						newIssue.fields[key] = value;
+					}
+				}
+			}
+
+			data = JSON.stringify(newIssue);
+
+			$.ajax({
+				url: API_URL,
+				type: "POST",
+				data: data,
+				contentType: "application/json; charset=utf-8",
+				dataType: "json",
+				success: function (data, textStatus, jqXHR) {
+					console.log('[jira-issue-clone][createNewIssue] ' + data);
+
+					window.location = BROWSE_URL + data.key;
+				}
+			}).error(handleAjaxError);
+		}
+		catch (err) {
+			alert(err.message);
+		}
+	};
+
+	/**
+	* Manipulate values, e.g. customfield_10005 in our case is not wanted during clone so setting the value to null so that it won't be cloned
+	*/
+	function customizeValue(key, value) {
+		var result = value;
+
+		if ("customfield_10005" === key) { // sprint id
+			result = null;
+		};
+
+		return result;
+	}
+
+	/**
+	* Central error handler for ajax requests.
+	*/
+	function handleAjaxError(jqXHR, exception, errorThrown) {
+
+		var msg = '';
+
+		if (jqXHR.status === 0) {
+			msg = 'Not connected. Verify Network.';
+		} else if (jqXHR.status == 404) {
+			msg = 'Requested page not found. [404]';
+		} else if (jqXHR.status == 500) {
+			msg = 'Internal Server Error [500].';
+		} else if (exception === 'parsererror') {
+			msg = 'Requested JSON parse failed.';
+		} else if (exception === 'timeout') {
+			msg = 'Time out error.';
+		} else if (exception === 'abort') {
+			msg = 'Ajax request aborted.';
+		} else {
+			msg = 'Uncaught Error.\n' + jqXHR.responseText;
+		};
+
+		console.log("[jira-issue-clone][handleAjaxError] jQuery XHR Exception. Status: " + jqXHR.status + ". Exception: " + exception + ". Information: " + msg + ". Error Thrown" + errorThrown );
+		
+		alert('An error occurred. Look at the console (F12 or Ctrl+Shift+I, Console tab) for more information.');
+	};
+
+	// Add the clone button if an issue is refreshed, because a refresh will remove the clone button added by the NEW_CONTENT_ADDED event 
+	JIRA.bind(JIRA.Events.ISSUE_REFRESHED, function (context, issueId) {
+		console.log('[jira-issue-clone][ISSUE_REFRESHED]: Adding clone button');
+		addCloneButton();
+	});
+
+	//Add the clone button if an issue is opened, using the pageLoad for an issue-container 
+	JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function (e, context, reason) {
+		if ('pageLoad' === reason && context.selector.includes('.issue-container')) {
+			console.log('[jira-issue-clone][NEW_CONTENT_ADDED]: Adding clone button');
+			addCloneButton();
+		}
+	});
 };
 
 /**
-* Main function that is execute when user clicks the clone button.
-* Drives the process of retrieving info, building a new issue and submitting it.
-*/
-function cloneIssue() {
-  try {
-    var issueId = $("meta[name='ajs-issue-key']").attr("content");
-
-    getCurrentIssueFields(issueId)
-    .then(function(issue){
-        var currentFields = issue.fields;        
-        var projectId = currentFields.project.id;
-        var issueTypeId = currentFields.issuetype.id;
-        
-        getIssueTypeFields(projectId, issueTypeId)
-        .then(function(meta) {            
-            var issueTypeFields = meta.projects[0].issuetypes[0].fields;          
-            createNewIssue(currentFields, issueTypeFields);          
-        })
-        .error(handleJsonError);
-    })
-    .error(handleJsonError);
-  }
-  catch(err) {
-    alert(err.message);
-  }  
-};
-
-/**
-* Returns a promise to load the data of the current issue.
-* @param {string} issueId - id of the issue to retrieve
-*/
-function getCurrentIssueFields(issueId) {
-    try {
-      var query = API_URL + issueId;    
-      return $.getJSON(query);
-    }
-     catch(err) {
-      alert(err.message);
-    }     
-};
-
-/**
-* Returns a promise to load the fields on the create screen for a certain project and issue type.
-* @param {string} projectId - id of the project to find the Create screen.
-* @param {string} issueTypeId - id of the issue type to find the Create screen
-*/
-function getIssueTypeFields(projectId, issueTypeId) {
-    try {
-      var query = API_URL + "createmeta?projectIds="+ projectId +"&issuetypeIds="+ issueTypeId +"&expand=projects.issuetypes.fields";
-      return $.getJSON(query);
-    }
-     catch(err) {
-     alert(err.message);
-    }  
-};
-
-/**
-* Builds a new issue using information of the current issue, filtered down with the fields on the create screen.
-* @param {string} currentFields - list of fields of the current issue.
-* @param {string} issueTypeFields - list of fields on the Create screen for this issue type
-*/
-function createNewIssue(currentFields, issueTypeFields) {
-
-    try {
-        var newIssue = {
-            "fields": {
-            "issuetype": {
-                "id": currentFields.issuetype.id
-            },
-            "priority": {
-                "id": currentFields.priority.id
-            },
-            "project": {
-                "id": currentFields.project.id
-            },
-            "summary": currentFields.summary,
-            "description": currentFields.description,
-            }
-        };
-
-        //Custom fields    
-        for (var key in issueTypeFields) {
-            if (key.startsWith('customfield_') && currentFields.hasOwnProperty(key)) {
-              
-                var value = customizeValue(key, currentFields[key])
-                 
-                if(value) {
-                  newIssue.fields[key]=value;
-                }
-            }
-        }        
-
-        data = JSON.stringify(newIssue);
-
-        $.ajax({
-            url: API_URL,
-            type: "POST",
-            data: data,
-            contentType:"application/json; charset=utf-8",
-            dataType:"json",
-            success: function(data, textStatus, jqXHR) {
-                console.log(data);
-                console.log('jqXHR:');
-                console.log(jqXHR);
-                console.log('textStatus:');
-                console.log(textStatus);
-                console.log('data:');
-                console.log(data);
-
-                window.location = BROWSE_URL + data.key;
-            }
-        }).error(handleJsonError);
-    }
-    catch(err) {
-        alert(err.message);
-    }    
-};
-
-/**
-* Manipulate values, e.g. customfield_10005 in our case is not wanted during clone so setting the value to null so that it won't be cloned
-*/
-function customizeValue(key, value) {
-  var result = value;
-  
-  if("customfield_10005" === key) { // sprint id
-   result = null;
-  };
-  
-  return result;
-}
-
-/**
-* Central error handler for ajax requests.
-*/
-function handleJsonError(jqXHR, textStatus, errorThrown) {
-  alert('An error occurred. Look at the console (F12 or Ctrl+Shift+I, Console tab) for more information.');
-  console.log('jqXHR:');
-  console.log(jqXHR);
-  console.log('textStatus:');
-  console.log(textStatus);
-  console.log('errorThrown:');
-  console.log(errorThrown);
-};
+ *Main function to inject the code of the jira-issue-clone extension into the JIRA website
+ */
+(function (callback) {
+	'use strict';
+	try {
+		var scripts = "(" + callback.toString() + ")(window.AJS.$);";
+		injectsome.content.script(scripts);
+	} catch {
+		console.log('[jira-issue-clone] ' + err.message);
+	}
+})(scriptToInject);
